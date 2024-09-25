50742a56a5d44457b9e25ac44b937124f64bbb09
Miika <vainiomiika@hotmail.com>
2024-09-25 21:35:15 +0300

    Adding the 7 labels needed

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app.title("NHL GameAdr")
 window_width = 400
 window_height = 315
 app.geometry(f"{window_width}x{window_height}")
+app.resizable(False, False)
 
 screen_width = app.winfo_screenwidth()
 screen_height = app.winfo_screenheight()
@@ -18,6 +19,37 @@ x = int((screen_width / 2) - (window_width / 2))
 y = int((screen_height / 2) - (window_height / 2))
 app.geometry(f"{window_width}x{window_height}+{x}+{y}")
 # https://www.geeksforgeeks.org/how-to-center-a-window-on-the-screen-in-tkinter/
+
+# Input fields
+
+tk.Label(app, text="Date (DD-MM-YYYY)").grid(row=0, column=0, sticky="e")
+date_entry = tk.Entry(app)
+date_entry.grid(row=0, column=1, columnspan=2)
+
+tk.Label(app, text="Start Time (HH:MM)").grid(row=1, column=0, sticky="e")
+date_entry = tk.Entry(app)
+date_entry.grid(row=1, column=1, columnspan=2)
+
+tk.Label(app, text="Home Team").grid(row=2, column=0, sticky="e")
+date_entry = tk.Entry(app)
+date_entry.grid(row=2, column=1, columnspan=2)
+
+tk.Label(app, text="Visitor Team").grid(row=3, column=0, sticky="e")
+date_entry = tk.Entry(app)
+date_entry.grid(row=3, column=1, columnspan=2)
+
+tk.Label(app, text="Home Score").grid(row=4, column=0, sticky="e")
+date_entry = tk.Entry(app)
+date_entry.grid(row=4, column=1, columnspan=2)
+
+tk.Label(app, text="Visitor Score").grid(row=5, column=0, sticky="e")
+date_entry = tk.Entry(app)
+date_entry.grid(row=5, column=1, columnspan=2)
+
+tk.Label(app, text="Status").grid(row=6, column=0, sticky="e")
+date_entry = tk.Entry(app)
+date_entry.grid(row=6, column=1, columnspan=2)
+
 
 # Starts the loop
 app.mainloop()
