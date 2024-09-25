dcc1ba0a2e4816c98bdcc76d99ce1d457a8944e3
Miika <vainiomiika@hotmail.com>
2024-09-25 20:27:16 +0300

    tkinter start, window creation

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import csv
 from datetime import datetime
+
+# Creates the window, title and size
+app = tk.Tk()
+app.title("NHL GameAdr")
+app.geometry("400x315")
+
+
+# Starts the loop
+app.mainloop()
