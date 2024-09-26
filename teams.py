405fbf0f7345e071caaff31a7959a0f636e9db74
Miika <vainiomiika@hotmail.com>
2024-09-26 18:23:37 +0300

    NHL teams for 2024-2025

---
+++ teams.py
@@ -0,0 +1,10 @@
+teams = [
+    "Buffalo Sabres", "Detroit Red Wings", "Montreal Canadiens", "Ottawa Senators",
+    "Tampa Bay Lightning", "Toronto Maple Leafs", "Florida Panthers", "Boston Bruins",
+    "Carolina Hurricanes", "New Jersey Devils", "New York Islanders", "New York Rangers",
+    "Philadelphia Flyers", "Pittsburgh Penguins", "Washington Capitals", "Columbus Blue Jackets",
+    "Utah Hockey Club", "Chicago Blackhawks", "Dallas Stars", "Colorado Avalanche",
+    "St. Louis Blues", "Nashville Predators", "Winnipeg Jets", "Minnesota Wild",
+    "Edmonton Oilers", "Los Angeles Kings", "San Jose Sharks", "Vancouver Canucks",
+    "Anaheim Ducks", "Vegas Golden Knights", "Seattle Kraken", "Calgary Flames"
+]
